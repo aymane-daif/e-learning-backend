236694c3e535d92d99b014846788249e22af3201
aymane-daif <aymaned345@gmail.com>
2022-12-23 23:16:25 +0100

    :bug: fix cors config

--- api-gateway-service/src/main/java/ma/ensa/apigatewayservice/config/SecurityConfig.java
+++ api-gateway-service/src/main/java/ma/ensa/apigatewayservice/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -23,7 +26,14 @@ public class SecurityConfig {
                                 .anyExchange()
                                 .permitAll())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-
+        serverHttpSecurity
+                .cors().configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+                    configuration.setAllowedMethods(List.of("*"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    return configuration;
+                });
         return serverHttpSecurity.build();
     }
 
