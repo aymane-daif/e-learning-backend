a951a45e7fd9bcb528121b2d2090e097a20c356f
abdelali4 <abdelali.elhammadi@gmail.com>
2023-01-07 22:18:55 +0100

    certification service is nearly finished

---
+++ certification_service/src/main/java/com/elearning/certification_service/repositories/CertificationRepository.java
@@ -0,0 +1,9 @@
+package com.elearning.certification_service.repositories;
+
+import com.elearning.certification_service.entities.Certificate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CertificationRepository extends JpaRepository<Certificate, String> {
+}
