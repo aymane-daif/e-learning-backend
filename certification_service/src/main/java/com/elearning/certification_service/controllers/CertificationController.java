d28c25636ca59b9e9456de35a22ba9122ac2af42
Abdelali EL hammadi <79413375+abdelali4@users.noreply.github.com>
2023-01-09 13:02:55 +0100

    Update CertificationController.java

--- certification_service/src/main/java/com/elearning/certification_service/controllers/CertificationController.java
+++ certification_service/src/main/java/com/elearning/certification_service/controllers/CertificationController.java
@@ -5,7 +5,6 @@ import com.elearning.certification_service.dtos.CertificateDto;
 import com.elearning.certification_service.exceptions.CertificateDoesntExist;
 import com.elearning.certification_service.services.CertificationService;
 
-<<<<<<< HEAD
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,15 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-<<<<<<< HEAD
-=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
->>>>>>> a423fc4393db1660d729a6656b03bb1f4427c60a
-=======
 
->>>>>>> afb696dc2d9e99c3093b2949b011e4d677a5d24c
 
 @RestController
 @RequestMapping(path = "/certifications")
@@ -31,20 +25,14 @@ public class CertificationController {
     @Autowired
     private CertificationService certificationService;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     @GetMapping("/hola")
     public String createCertificate(@RequestBody CertificateDto certificateDto){
         certificationService.createCertificate(certificateDto);
         return "hola";
-=======
-=======
 
->>>>>>> afb696dc2d9e99c3093b2949b011e4d677a5d24c
     @GetMapping("/")
     public void createCertificate(@RequestBody CertificateDto certificateDto) {
         certificationService.createCertificate(certificateDto);
->>>>>>> a423fc4393db1660d729a6656b03bb1f4427c60a
     }
 
     @GetMapping("/{id}")
